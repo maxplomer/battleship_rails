98f6d112927f88c3b8ea950c8c53009feac1b990
Max Plomer <maxplomer@gmail.com>
2016-08-05 17:48:22 -0400

    add email

--- db/schema.rb
+++ db/schema.rb
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20160805054426) do
 
   create_table "users", force: :cascade do |t|
     t.string   "auth0id"
+    t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["auth0id"], name: "index_users_on_auth0id", unique: true, using: :btree
