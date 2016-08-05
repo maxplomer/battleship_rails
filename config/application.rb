6193ebbfa68847ade4a58d042295728bfb8b8d2e
Max Plomer <maxplomer@gmail.com>
2016-08-05 15:47:29 -0400

    allow patch

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module BattleshipRails
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options, :delete]
+        resource '*', :headers => :any, :methods => [:get, :post, :options, :delete, :patch]
       end
     end
     
